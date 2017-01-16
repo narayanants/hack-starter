7ce88042a71925f1dec9dd6565f713d2fdf50aa1
Narayanan Ts <tsnarayanan7@gmail.com>
2017-01-16 21:55:06 +0530

    Added  Oauth strategy

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,8 @@
 const _ = require('lodash');
 const passport = require('passport');
 const request = require('request');
-const LocalStrategy = require('passport-local').Strategy;
 const InstagramStrategy = require('passport-instagram').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const TwitterStrategy = require('passport-twitter').Strategy;
 const GitHubStrategy = require('passport-github').Strategy;
@@ -14,32 +14,46 @@ const OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
 
 const User = require('../models/User');
 
-passport.serializeUser((user,done)=>{
-    done(null,user.id);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
 });
 
-passport.deserializeUser((id,done)=>{
-    User.findById(id,(err,user)=>{
-        done(err,user);
-    });
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
 });
 
-/* Sign in using email and password */
-
-passport.use(new LocalStrategy({usernameField:'email'},(email,password,done)=>{
-    User.findOne({email:email.toLowerCase()},(err,user)=>{
-        if(err) {return done(err);}
-        if(!user){
-            return done(null,false,{msg:`Email ${email} not found!`});
-        }
-        user.comparePassword(password,(err,isMatch)=>{
-            if(err) { return done(err);}
-            if(isMatch){
-                return done(null,user);
-            }
-            return done(null,false,{msg:'Invalid email or password'});
-        });
+/**
+ * Sign in using Email and Password.
+ */
+passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  User.findOne({ email: email.toLowerCase() }, (err, user) => {
+    if (err) { return done(err); }
+    if (!user) {
+      return done(null, false, { msg: `Email ${email} not found.` });
+    }
+    user.comparePassword(password, (err, isMatch) => {
+      if (err) { return done(err); }
+      if (isMatch) {
+        return done(null, user);
+      }
+      return done(null, false, { msg: 'Invalid email or password.' });
     });
+  });
 }));
 
-
+/**
+ * OAuth Strategy Overview
+ *
+ * - User is already logged in.
+ *   - Check if there is an existing account with a provider id.
+ *     - If there is, return an error message. (Account merging not supported)
+ *     - Else link new OAuth account with currently logged-in user.
+ * - User is not logged in.
+ *   - Check if it's a returning user.
+ *     - If returning user, sign in and we are done.
+ *     - Else check if there is an existing account with user's email.
+ *       - If there is, return an error message.
+ *       - Else create a new account.
+ */
