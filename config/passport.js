c27ff25703467dca80962036381ae0cbf99e9df0
Narayanan Ts <tsnarayanan7@gmail.com>
2017-01-17 20:44:13 +0530

    Added Tumblr Oauth

--- config/passport.js
+++ config/passport.js
@@ -391,11 +391,31 @@ passport.use(new InstagramStrategy({
         // to a valid e-mail address in Profile Management.
         user.email = `${profile.username}@instagram.com`;
         user.profile.website = profile._json.data.website;
-        user.profile.picture = profile._json.data.profile.picture;
+        user.profile.picture = profile._json.data.profile_picture;
         user.save((err)=>{
           done(err,user);
         });
       }
     }); 
   }
+}));
+
+/* Tumblr Oauth API */
+
+passport.use('tumblr',new OAuthStrategy({
+  requestTokenURL: 'https://www.tumblr.com/oauth/request_token',
+  accessTokenURL: 'https://www.tumblr.com/oauth/access_token',
+  userAuthorizationURL: 'https://www.tumblr.com/oauth/authorize',
+  consumerKey: process.env.TUMBLR_KEY,
+  consumerSecret: process.env.TUMBLR_SECRET,
+  callbackURL: '/auth/tumblr/callback',
+  passReqToCallback: true
+},(req,token,tokenSecret,profile,done)=>{
+  User.findById(req.user._id,(err,user)=>{
+    if(err){return done(err);}
+    user.tokens.push({kind:'tumblr',accessToken: token, tokenSecret});
+    user.save((err)=>{
+      done(err,user);
+    });
+  });
 }));
