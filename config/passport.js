4e9e03a8a44c7473ba5bafefeb4c6fba6ff324bc
Narayanan Ts <tsnarayanan7@gmail.com>
2017-01-17 20:44:39 +0530

    Added Tumblr Oauth

--- config/passport.js
+++ config/passport.js
@@ -402,20 +402,22 @@ passport.use(new InstagramStrategy({
 
 /* Tumblr Oauth API */
 
-passport.use('tumblr',new OAuthStrategy({
-  requestTokenURL: 'https://www.tumblr.com/oauth/request_token',
-  accessTokenURL: 'https://www.tumblr.com/oauth/access_token',
-  userAuthorizationURL: 'https://www.tumblr.com/oauth/authorize',
+passport.use('tumblr', new OAuthStrategy({
+  requestTokenURL: 'http://www.tumblr.com/oauth/request_token',
+  accessTokenURL: 'http://www.tumblr.com/oauth/access_token',
+  userAuthorizationURL: 'http://www.tumblr.com/oauth/authorize',
   consumerKey: process.env.TUMBLR_KEY,
   consumerSecret: process.env.TUMBLR_SECRET,
   callbackURL: '/auth/tumblr/callback',
   passReqToCallback: true
-},(req,token,tokenSecret,profile,done)=>{
-  User.findById(req.user._id,(err,user)=>{
-    if(err){return done(err);}
-    user.tokens.push({kind:'tumblr',accessToken: token, tokenSecret});
-    user.save((err)=>{
-      done(err,user);
+},
+  (req, token, tokenSecret, profile, done) => {
+    User.findById(req.user._id, (err, user) => {
+      if (err) { return done(err); }
+      user.tokens.push({ kind: 'tumblr', accessToken: token, tokenSecret });
+      user.save((err) => {
+        done(err, user);
+      });
     });
-  });
-}));
+  }
+));
