cb0e557f26db229d092fe9a4821d67f3136cbf72
Narayanan Ts <tsnarayanan7@gmail.com>
2017-01-25 11:56:58 +0530

    Added mongoose connection

--- app.js
+++ app.js
@@ -36,6 +36,16 @@ const passportConfig = require('./config/passport');
 /* Create Express server */
 var app = express();
 
+/**
+ * Connect to MongoDB.
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connection.on('error', () => {
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+  process.exit();
+});
+
 /* Express Configuration */
 app.set('port',process.env.PORT || 3000);
 app.set('view engine','pug');
