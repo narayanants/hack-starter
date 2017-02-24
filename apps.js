5e542e0ac29e7370fac3f8a74354dcdb0ab7fbbb
Narayanan Ts <tsnarayanan7@gmail.com>
2017-02-24 19:45:06 +0530

    API examples

--- apps.js
+++ apps.js
@@ -106,18 +106,53 @@ app.get('/login',userController.getLogin);
 app.post('/login',userController.postLogin);
 app.get('/logout',userController.getLogout);
 app.get('/forgot',userController.getForgot);
-app.post('/forgot',userController.postForgot);
+app.post('/forogot',userController.postForgot);
 app.get('/reset/:token',userController.getReset);
 app.post('/reset/:token',userController.postReset);
 app.get('/signup',userController.getSignup);
 app.post('/signup',userController.postSignup);
 app.get('/contact',contactController.getContact);
-app.get('/contact',contactController.postContact);
-app.get('/account',passportConfig.isAuthenticated,userController.getAccount);
+app.post('/contact',contactController.postContact);
+app.get('/account',passportConfig.getAccount);
 app.post('/account/profile',passportConfig.isAuthenticated,userController.postUpdateProfile);
 app.post('/account/password',passportConfig.isAuthenticated,userController.postUpdatePassword);
 app.post('/account/delete',passportConfig.isAuthenticated,userController.postDeleteAccount);
-app.post('/account/unlin/:provider',passportConfig.isAuthenticated,userController.getOauthUnlink);
+app.get('/unlink/:provider',passportConfig.isAuthenticated,userController.getOauthUnlink);
+
+/* API examples routes */
+
+app.get('/api',apiController.getApi);
+app.get('/api/lastfm',apiController.getLastfm);
+app.get('/api/nyt',apiController.getNewYorkTimes);
+app.get('/api/aviary',apiController.getAviary);
+app.get('/api/steam',passportConfig.isAuthenticated,passportConfig.isAuthorized,apiController.getSteam);
+app.get('/api/stripe',apiController.getStripe);
+app.post('/api/stripe',apiController.postStripe);
+app.get('/api/scraping',apiController.getScraping);
+app.get('/api/twilio',apiController.getTwilio);
+app.post('/api/twilio',apiController.postTwilio);
+app.get('/api/clockwork',apiController.getClockwork);
+app.post('/api/clockwork',apiController.postClockwork);
+app.get('/api/foursquare',passportConfig.isAuthenticated,passportConfig.isAuthorized,apiController.getFoursquare);
+app.get('/api/tumblr',passportConfig.isAuthenticated,passportConfig.isAuthorized,apiController.getTumblr);
+app.get('/api/facebook',passportConfig.isAuthenticated,passportConfig.isAuthorized,apiController.getFacebook);
+app.get('/api/github',passportConfig.isAuthenticated,passportConfig.isAuthorized,apiController.getGithub);
+app.get('/api/twitter',passportConfig.isAuthenticated,passportConfig.isAuthorized,apiController.getTwitter);
+app.post('/api/twitter',passportConfig.isAuthenticated,passportConfig.isAuthorized,apiController.postTwitter);
+app.get('/api/linkedin',passportConfig.isAuthenticated,passportConfig.isAuthorized,apiController.getLinkedin);
+app.get('/api/instagram',passportConfig.isAuthenticated,passportConfig.isAuthorized,apiController.getInstagram);
+app.get('/api/paypal',apiController.getPaypal);
+app.get('/api/paypal/success',apiController.getPaypalSuccess);
+app.get('/api/paypal/cancel',apiController.getPaypalCancel);
+app.get('/api/lob',apiController.getLob);
+app.get('/api/upload',apiController.getUpload);
+app.post('/api/upload',upload.single('myFile'),apiController.postFileUpload);
+app.get('/api/pinterest',passportConfig.isAuthenticated,passportConfig.isAuthorized,apiController.getPinterest);
+app.post('/api/pinterest',passportConfig.isAuthenticated,passportConfig.isAuthorized,apiController.postPinterest);
+app.get('/api/google-maps',apiController.getGoogleMaps);
+
+
+
 
 
 
