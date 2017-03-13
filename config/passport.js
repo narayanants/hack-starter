e7fda8408952bcbe4edfb52766282307bdd511af
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-13 10:50:57 +0530

    added github strategy

--- config/passport.js
+++ config/passport.js
@@ -58,17 +58,17 @@ passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_ID,
   clientSecret: process.env.FACEBOOK_SECRET,
   callbackURL: '/auth/facebook/callback',
-  profileFields: ['email','profile','name','link','locale','timezone'],
-  passReqToCallback:true
+  profileFields: ['name', 'email', 'link', 'locale', 'timezone'],
+  passReqToCallback:true,
 },(req,refreshToken,accessToken,profile,done)=>{
   if(req.user){
     User.findOne({facebook:profile.id},(err,existingUser)=>{
       if(err){return done(err);}
       if(existingUser){
-       req.flash('errors', { msg: 'There is already a Facebook account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
-       done(err);
+         req.flash('errors', { msg: 'There is already a Facebook account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+         done(err);
       }else{
-        user.findById(req.user.id,(err,user)=>{
+        User.findById(req.user.id,(err,user)=>{
           if(err){return done(err);}
           user.facebook = profile.id;
           user.tokens.push({kind:'facebook',accessToken});
@@ -83,27 +83,90 @@ passport.use(new FacebookStrategy({
       }
     });
   }else{
-    User.findOne({facebook:req.id},(err,existingUser)=>{
-      if(err){return done(err);}  
-      if (existingUser) {
-        return done(null, existingUser);
+    User.findOne({facebook:profile.id},(err,existingUser)=>{
+      if(err){return done(err);}
+      if(existingUser){
+        return done(null,existingUser);
       }
       User.findOne({email:profile._json.email},(err,existingEmailUser)=>{
-        if(err){return done(err);} 
-        if (existingEmailUser) {
+        if(err){return done(err);}
+        if(existingEmailUser){
           req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Facebook manually from Account Settings.' });
           done(err);
         }else{
           const user = new User();
-          user.email = profile._json.email;
           user.facebook = profile.id;
-          user.tokens.push({kind:'facebook',accessToken});
+          user.email = profile._json.email;
+          user.tokens.push({ kind: 'facebook', accessToken });
           user.profile.name = `${profile.givenName} ${profile.displayName}`;
           user.profile.gender = profile._json.gender;
           user.profile.picture = `https://graph.facebook.com/${profile.id}/picture?type=large`;
-          user.profile.location = (profile._json.location) ? profile._json.location.name : '';
+          user.profile.location = (profile._json.location)? profile._json.location.name : '';
           user.save((err)=>{
             done(err,user);
+          }); 
+        }
+      });
+    });
+  }
+}));
+
+
+/**
+ * Sign in with GitHub.
+ */
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_ID,
+  clientSecret: process.env.GITHUB_SECRET,
+  callbackURL: '/auth/github/callback',
+  passReqToCallback: true
+},(req,refreshToken,accessToken,profile,done)=>{
+  if(req.user){
+    User.findOne({github:profile.id},(err,existingUser)=>{
+       if(err){return done(err);}
+       if(existingUser){
+         req.flash('errors', { msg: 'There is already a Facebook account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+         done(err);
+       }else{
+         User.findById(req.user.id,(err,user)=>{
+            if(err){return done(err);}
+            user.github = profile.id;
+            user.tokens.push({ kind: 'github', accessToken });
+            user.profile.name = user.profile.name || profile.displayName;
+            user.profile.picture = user.profile.picture || profile._json.avatar_url;
+            user.profile.location = user.profile.location || profile._json.location;
+            user.profile.website = user.profile.website || profile._json.blog;
+            user.save((err)=>{
+              req.flash('info', { msg: 'GitHub account has been linked.' });
+              done(null,user);
+            });
+         });
+       }
+    });
+  }else{
+    User.findOne({github:profile.id},(err,existingUser)=>{
+      if(err){return done(err);}
+      if(existingUser){
+        return done(null,existingUser);
+      }
+      User.findOne({email:profile._json.email},(err,existingEmailUser)=>{
+        if(err){return done(err);}
+        if(existingEmailUser){
+          req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with GitHub manually from Account Settings.' });
+          done(err);
+        }else{
+          const user = new User();
+          user.email = profile._json.email;
+          user.github = profile.id;
+          user.tokens.push({kind:'github',accessToken});
+          user.profile.name = profile.displayName;
+          user.profile.picture = profile._json.avatar_url;
+          user.profile.location = profile._json.location;
+          user.profile.website = profile._json.blog;
+          user.save((err)=>{
+            req.flash('info',{msg:'Github account has been linked!'});
+            return done(err,user);
           });
         }
       });
