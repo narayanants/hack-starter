d5cfa4a88ba45ab3659fc6538ae8fcc2772be7e0
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-13 11:11:37 +0530

    added google strategy

--- config/passport.js
+++ config/passport.js
@@ -173,3 +173,111 @@ passport.use(new GitHubStrategy({
     });
   }
 }));
+
+// Sign in with Twitter.
+
+passport.use(new TwitterStrategy({
+  consumerKey: process.env.TWITTER_KEY,
+  consumerSecret: process.env.TWITTER_SECRET,
+  callbackURL: '/auth/twitter/callback',
+  passReqToCallback: true,
+},(req,tokenSecret,accessToken,profile,done)=>{
+  if(req.user){
+    User.findOne({twitter:profile.id},(err,existingUser)=>{
+      if(err){return done(err);}
+      if(existingUser){
+         req.flash('errors', { msg: 'There is already a Twitter account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+        done(err);
+      }else{
+        User.findById(req.user.id,(err,user)=>{
+           if(err){return done(err);}
+           user.twitter = profile.id;
+           user.tokens.push({kind:'twitter',accessToken});
+           user.profile.name = user.profile.name || profile.displayName;
+           user.profile.location = user.profile.location || profile._json.location;
+           user.profile.picture = user.profile.picture || profile._json.image_url_https;
+           user.save((err)=>{
+             req.flash('info', { msg: 'Twitter account has been linked.' });
+             done(err,user);
+           });
+        }); 
+      }
+    });
+  }else{
+    User.findOne({twitter:profile.id},(err,existingUser)=>{
+      if(err){return done(err);}
+      if(existingUser){
+        return done(null,existingUser);
+      }
+      const user = new User();
+      user.name = `${profile.username}@twitter.com`;
+      user.twitter = profile.id;
+      user.tokens.push({kind:'twitter',accessToken,tokenSecret});
+      user.profile.name = profile.displayName;
+      user.profile.location = profile._json.location;
+      user.profile.picture = profile._json.profile_image_url_https;
+      user.save((err)=>{
+        done(err,user);
+      });
+    });
+  }
+}));
+
+
+
+// Sign in using GoogleStrategy
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: '/auth/google/callback',
+  passReqToCallback: true
+},(req,refreshToken,accessToken,profile,done)=>{
+  if(req.user){
+    User.findOne({google:profile.id},(err,existingUser)=>{
+      if(err){return done(err);}
+      if(existingUser){
+        req.flash('errors', { msg: 'There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+        done(err);
+      }else{
+        User.findById(req.user.id,(err,user)=>{
+          if(err){return done(err);}
+          user.google = profile.id;
+          user.tokens.push({kind:'google',accessToken});
+          user.profile.name = user.profile.name || profile.displayName;
+          user.profile.gender = user.profile.gender || profile._json.gender;
+          user.profile.picture = user.profile.picture || profile._json.image.url;
+          user.save((err)=>{
+            req.flash('info', { msg: 'Google account has been linked.' });
+            done(err,user);
+          });
+        });
+      }
+    });
+  }else{
+    User.findOne({google:profile.id},(err,existingUser)=>{
+      if(err){return done(err);}
+      if(existingUser){
+        return done(null,existingUser);
+      }
+      User.findOne({email:profile.emails[0].value},(err,existingEmailUser)=>{
+        if(err){return done(err);}
+        if(existingEmailUser){
+          req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Google manually from Account Settings.' });
+          done(err);
+        }else{
+          const user = new User();
+          user.email = profile._json.emails[0].value;
+          user.google = profile.id;
+          user.tokens.push({kind:'google',accessToken});
+          user.profile.name = profile.displayName;
+          user.profile.gender = profile._json.gender;
+          user.profile.picture = profile._json.image.url;
+          user.save((err)=>{
+            done(err, user);
+          });
+        }
+      });
+    });
+  }
+}));
