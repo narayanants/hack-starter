043c26b04155ced3d0510583d7157a64d69e5e3e
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-14 21:25:10 +0530

    added instagram strategy if uer is present

--- config/passport.js
+++ config/passport.js
@@ -342,3 +342,37 @@ passport.use(new LinkedInStrategy({
       });
     }
 }));
+
+/* Instagram Strategy */
+
+passport.use(new InstagramStrategy({
+  clientID: process.env.INSTAGRAM_ID,
+  clientSecret: process.env.INSTAGRAM_SECRET,
+  callbackURL: '/auth/instagram/callback',
+  passReqToCallback:true
+},(req,refreshToken,accessToken,profile,done)=>{
+  if(req.user){
+    User.findOne({instagram:profile.id},(err,existingUser)=>{
+      if(err){return done(err);}
+      if(existingUser){
+        req.flash('errors', { msg: 'There is already an Instagram account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+        done(err);
+      }else{
+        User.findById(req.user.id,(err,user)=>{
+          if(err){return done(err);}
+          user.instagram = profile.id;
+          user.tokens.push({kind:'instagram',accessToken});
+          user.profile.name  = user.profile.name || profile.displayName;
+          user.profile.picture = user.profile.picture || profile._json.data.profile_picture;
+          user.profile.website = user.profile.website || profile._json.data.website;
+          user.save((err)=>{
+            req.flash('info', { msg: 'Instagram account has been linked.' });
+            done(err,user);
+          }); 
+        });
+      }
+    });
+  }else{
+    User.findOne({instagram:})
+  }
+}));
