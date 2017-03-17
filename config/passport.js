01c12fe7f474d24cce6e3b36d24a26856baaa092
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-17 19:59:47 +0530

    added pinterest strategy

--- config/passport.js
+++ config/passport.js
@@ -468,3 +468,22 @@ passport.use(new OpenIDStrategy({
     });
   });
 })); 
+
+/* Pinterest API OAuth*/
+
+passport.use('pinterest',new OAuth2Strategy({
+  authorizationURL: 'https://api.pinterest.com/oauth',
+  tokenURL: 'https://api.pinterest.com/v1/oauth/token',
+  clientID: process.env.PINTEREST_ID,
+  clientSecret: process.env.PINTEREST_SECRET,
+  callbackURL: process.env.PINTEREST_REDIRECT_URL,
+  passReqToCallback:true
+},(req,refreshToken,accessToken,profile,done)=>{
+  User.findById(req.user._id,(err,user)=>{
+    if(err){return done(err);}
+    user.tokens,push({kind:'pinterest',accessToken});
+    user.save((err)=>{
+      done(err,user);
+    });
+  });
+}));
