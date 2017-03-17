f634a57b82851ed28a9043bf935eeaf36839d1d6
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-17 21:31:23 +0530

    added Authorization Required Middleware

--- config/passport.js
+++ config/passport.js
@@ -497,7 +497,15 @@ exports.isAuthenticated = (req,res,next)=>{
   res.redirect('/login');
 }
 
+/* Authorization Required Middleware */
 
-
+exports.isAuthorized = (req,res,next)=>{
+  const provider = req.path.split('/').slice(-1)[0];
+  if(_.find(req.user.tokens,{kind:provider})){
+    next();
+  }else{
+    res.redirect(`/auth/${provider}`);
+  }
+}
 
 
