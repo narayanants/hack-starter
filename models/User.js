eb4e323f0ffe7f2b042d24e079ccadd4b705ff10
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-20 20:35:57 +0530

    added user model

--- models/User.js
+++ models/User.js
@@ -1,43 +1,72 @@
 const bcrypt = require('bcrypt-nodejs');
 const crypto = require('crypto');
-const mongoose = require('mongoose');
+const mongoose = require('mognoose');
 
 const userSchema = new mongoose.Schema({
-    email: {type: String, unique:true},
+    email: {type:String, unique: true},
     password: String,
     passwordResetToken: String,
-    passwordResetExpires: Date,
+    passwordResetExpires: String,
 
     facebook: String,
-    twitter: String,
     google: String,
-    github: Stirng,
+    github: String,
     linkedin: String,
     instagram: String,
+    twitter: String,
     steam: String,
-    tokens:Array,
+    tokens: Array,
 
     profile:{
         name: String,
         gender: String,
+        picture: String,
         location: String,
-        website: String,
-        picture : String
+        website: String
     }
-},{timestamps: true});
+},{timestamps:true});
 
+/* Password hashing middleware */
 
-/* Password hash middleware */
-
-userSchema.pre('save',function save(err){
+userSchema.pre('save',function save(next){
     const user = this;
     if(!user.isModified('password')){return next();}
     bcrypt.genSalt(10,(err,salt)=>{
-        if(err){ return next(err);}
-        bcrypt.hash(user.password,(err,hash)=>{
+        if(err){return next(err);}
+        bcrypt.hash(user.password,salt,null,(err,hash)=>{
             if(err){return next(err);}
             user.password = hash;
             next();
         });
     });
 });
+
+/**
+ * Helper methods for validating user's password
+ */
+
+userSchema.methods.comparePassword = function comparePassword(candidatePassword,cb){
+    bcrypt.compare(user.password,this.password,(err,isMatch)=>{
+        cb(err,isMatch);
+    });
+};
+
+/**
+ * Helper for loading user's gravatar
+ */
+
+userSchema.methods.gravatar = function gravatar(size){
+    if(!size){
+        size = 200;
+    }
+    if(!this.email){
+        return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+    }
+
+    const md5 = crypto.createHash('md5').update(this.email).digest('hex');
+    return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
+};
+
+const User = mongoose.model('User',userSchema);
+
+module.exports = User;
