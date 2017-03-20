07facbf3da228c55f89322cb138ed9667ee67064
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-20 20:37:06 +0530

    added Helper method for getting user's gravatar.

--- models/User.js
+++ models/User.js
@@ -52,21 +52,19 @@ userSchema.methods.comparePassword = function comparePassword(candidatePassword,
 };
 
 /**
- * Helper for loading user's gravatar
+ * Helper method for getting user's gravatar.
  */
-
-userSchema.methods.gravatar = function gravatar(size){
-    if(!size){
-        size = 200;
-    }
-    if(!this.email){
-        return `https://gravatar.com/avatar/?s=${size}&d=retro`;
-    }
-
-    const md5 = crypto.createHash('md5').update(this.email).digest('hex');
-    return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
+userSchema.methods.gravatar = function gravatar(size) {
+  if (!size) {
+    size = 200;
+  }
+  if (!this.email) {
+    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+  }
+  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
+  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
 };
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
