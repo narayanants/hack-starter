4edb9c09dbf061e6551aeaaf316f725f1754ccaf
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-21 20:35:32 +0530

    added Signup page

--- controllers/user.js
+++ controllers/user.js
@@ -8,14 +8,14 @@ const User = require('../models/User');
 
 /**
  * GET /login
- * Login homepage
+ * Login Page.
  */
 
-exports.getLogin  = (req,res)=>{
+exports.getLogin = (req,res)=>{
     if(req.user){
         return res.redirect('/');
     }
-    res.render('account/login',{
+    res.render('login',{
         title: 'Login'
     });
 };
@@ -23,21 +23,55 @@ exports.getLogin  = (req,res)=>{
 
 /**
  * POST /login
- * Sign in using email and password
+ * sign in using email and password
  */
 
-exports.postLogin = (req,res)=>{
+exports.postLogin  = (req,res,next)=>{
     req.assert('email','Email is not valid').isEmail();
     req.assert('password','Password cannot be blank').notEmpty();
-    req.sanitize('email').normalizeUser({remove_dots:false});
+    req.sanitize('email').normalizeEmail({remove_dots:false});
 
     const errors = req.validationErrors();
     if(errors){
         req.flash('errors',errors);
-        return res.redirect('/login');
+        return res.redirectTo('/login');
     }
-
     passport.authenticate('local',(err,user,info)=>{
-        
+        if(err){return next(err);}
+        if(!user){
+            req.flash('errors',errors);
+            return res.redirect('/login');
+        }
+        req.logIn(user,(err)=>{
+            if(err){return next(err);}
+            req.flash('success','Success! You are logged in!');
+            res.redirect(req.session.returnTo || '/');
+        });
+    })(req,res,next);
+};  
+
+
+/**
+ * GET /logout
+ * Log out.
+ */
+
+exports.logout = (req,res)=>{
+    req.logout();
+    res.redirect('/');
+};
+
+/**
+ * GET /signup
+ * Signup page. 
+ */
+
+exports.getSignup = (req,res)=>{
+    if(req.user){
+        return res.redirect('/');
+    }
+    res.render('account/signup',{
+        title: 'Signup'
     });
 };
+
