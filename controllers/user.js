f6aa7b80b3a58e45a4b30b8537d26c8afa356a4a
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-24 20:28:50 +0530

    added Delete user account

--- controllers/user.js
+++ controllers/user.js
@@ -182,7 +182,19 @@ exports.postAccountPassword = (req,res,next)=>{
     });
 };
 
+/**
+ * POST /account/delete
+ * Delete user account
+ */
 
+exports.postDeleteAccount = (req,res,next)=>{
+    User.remove({_id:req.body.id},(err)=>{
+        if(err){return next(err);}
+        req.logout();
+        req.flash('info',{msg:'Your account has been deleted'});
+        res.redirect('/');
+    });
+};
 
 
 
