4fc6541d1eb9346c8e3c03df22fb70f79400454b
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-28 18:45:21 +0530

    added contact.js

--- controllers/contact.js
+++ controllers/contact.js
@@ -12,13 +12,13 @@ const transporter = nodemailer.createTransport({
 
 /**
  * GET /contact
- * Contact form page.
+ *  Contact form page
  */
 
 exports.getContact = (req,res)=>{
     res.render('contact',{
         title: 'Contact'
-    });
+    }); 
 };
 
 /**
@@ -27,21 +27,21 @@ exports.getContact = (req,res)=>{
  */
 
 exports.postContact = (req,res,next)=>{
-    req.assert('name','Name cannot be blank').notEmpty();   
-    req.assert('email','Email is not valid').isEmpty();
-    req.assert('message','Message cannot be blank').notEmpty();
+    req.assert('email','Email is not valid').notEmpty();
+    req.assert('name','Name cannot be empty').notEmpty();
+    req.assert('message','Message cannot be empty').notEmpty();
 
     const errors = req.validationErrors();
     if(errors){
-        req.flash('errors',errors);
+        res.flash('errors',errors);
         return res.redirect('/contact');
     }
 
     const mailOptions = {
-       to: 'your@email.com',
-       from: `${req.body.name} <${req.body.email}>`,
-       subject: 'Contact Form | Hackathon Starter',
-       text: req.body.message
+        to: 'your@email.com',
+        from :`${req.body.name} $<req.body.email>`,
+        subject: 'Contact Form | Hackathon Starter',
+        text: req.body.message
     };
 
     transporter.sendMail(mailOptions,(err)=>{
@@ -52,4 +52,4 @@ exports.postContact = (req,res,next)=>{
         req.flash('success', { msg: 'Email has been sent successfully!' });
         res.redirect('/contact');
     }); 
-};
+};  
